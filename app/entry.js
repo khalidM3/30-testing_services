49993f8bfc608a6ed8442036573046ab291b4b4d
Khalid Mohamud <kmohamud1@outlook.com>
2017-03-28 22:39:56 -0700

    lab-khalid

---
+++ app/entry.js
@@ -0,0 +1,47 @@
+'use strict';
+
+require('./scss/main.scss');
+
+const
+  path = require('path'),
+  angular = require('angular'),
+  camelcase = require('camelcase'),
+  pascalcase = require('pascalcase'),
+  uiRouter = require('angular-ui-router'),
+  ngTouch = require('angular-touch'),
+  ngAnimate = require('angular-animate'),
+
+  cfgram = angular.module('cfgram', [ngTouch, ngAnimate, uiRouter]);
+
+let context = require.context('./config/', true, /\.js$/);
+console.log('CONTEXT', context);
+context.keys().forEach( key => {
+  console.log('k',key);
+  cfgram.config(context(key));
+});
+
+context = require.context('./view/', true, /\.js$/);
+console.log('CONTEXT2', context);
+context.keys().forEach( key => {
+   console.log('k',key);
+  let name = pascalcase(path.basename(key, '.js'));
+  let module = context(key);
+  cfgram.controller(name, module);
+});
+
+context = require.context('./service/', true, /\.js$/);
+context.keys().forEach(key => {
+   console.log('k',key);
+  let name = camelcase(path.basename(key, '.js'));
+  let module = context(key);
+  cfgram.service(name, module);
+});
+
+context = require.context('./component', true, /\.js$/);
+context.keys().forEach( key => {
+   console.log('k',key);
+  let name = camelcase(path.basename(key, '.js'));
+  let module = context(key);
+  cfgram.component(name, module);
+})
+
